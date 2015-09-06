e6fa88cc04aa3dc7db31d61632aa4443f62751d6
Wesley <wesleys.felipe@gmail.com>
2015-09-06 16:26:47 -0300

    Entidade: Representante

---
+++ forcadevendas/src/main/java/com/pqt/forcadevendas/entity/Representante.java
@@ -0,0 +1,117 @@
+package com.pqt.forcadevendas.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "REPRESENTANTE", schema = "FDV_ONLINE") //TODO: remover conf de schema daqui
+public class Representante implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 255, nullable = false)
+	private String nome;
+	
+	@Column(length = 12, nullable = false)
+	private String cpf;
+	
+	@Column(length = 11, nullable = false)
+	private String rg;
+	
+	@Column(length = 255, nullable = false)
+	private String email;
+	
+	@Column(length = 25, nullable = false)
+	private String fone;
+	
+	@Column(nullable = false)
+	private Character situacao;
+	
+	@Column(length = 30, nullable = false)
+	private String login;
+	
+	@Column(length = 30, nullable = false)
+	private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public Character getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Character situacao) {
+		this.situacao = situacao;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
