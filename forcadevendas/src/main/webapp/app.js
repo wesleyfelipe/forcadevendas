ff0d5d96e3daf704f910c5a22a21e7eda89a9e14
nick1 <nicolas.cnunes@gmail.com>
2015-09-17 23:38:00 -0300

    Bootstrap do Angular

---
+++ forcadevendas/src/main/webapp/app.js
@@ -0,0 +1,11 @@
+(function(angular, window, undefined) {
+    window.ForcaDeVendas = {
+        app: angular.module('forcaDeVendas', [
+            'ngRoute'
+        ])
+    };
+
+    ForcaDeVendas.app.config(['$routeProvider', function($routeProvider) {
+        
+    }]);
+})(angular, window);
