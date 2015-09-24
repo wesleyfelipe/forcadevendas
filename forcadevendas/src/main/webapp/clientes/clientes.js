17de3301384e0d048e39fd6efc128e273b5ed120
nick1 <nicolas.cnunes@gmail.com>
2015-09-23 22:05:53 -0300

    Muda a página estática para um template do Angular

--- forcadevendas/src/main/webapp/clientes/clientes.js
+++ forcadevendas/src/main/webapp/clientes/clientes.js
@@ -1,7 +1,52 @@
 (function(angular) {
     var clientesModule = angular.module('forcaDeVendas.clientes', []);
 
-    clientesModule.controller('ClientesController', [function() {
+    clientesModule.controller('ClientesController', ['$scope', function($scope) {
+        var mockClients = [
+            {
+                id: '<Código>',
+                nome: '<Nome>',
+                status: '<Status>',
+                cnpj: '<CNPJ>'
+            },
+            {
+                id: '001',
+                nome: 'Cliente #001',
+                status: 'Ativo',
+                cnpj: '00.000.000/0001-00'
+            },
+            {
+                id: '002',
+                nome: 'Cliente #002',
+                status: 'Ativo',
+                cnpj: '00.000.000/0001-00'
+            },
+            {
+                id: '003',
+                nome: 'Cliente #003',
+                status: 'Inativo',
+                cnpj: '00.000.000/0001-00'
+            },
+            {
+                id: '004',
+                nome: 'Cliente #004',
+                status: 'Ativo',
+                cnpj: '00.000.000/0001-00'
+            }
+        ];
 
+        $scope.clientes = mockClients;
+
+        $scope.getStatusClass = function getStatusClass(status) {
+            if (status === 'Ativo') {
+                return 'label-success';
+            }
+
+            if (status === 'Inativo') {
+                return 'label-danger';
+            }
+
+            return 'label-default';
+        };
     }]);
 })(angular);
