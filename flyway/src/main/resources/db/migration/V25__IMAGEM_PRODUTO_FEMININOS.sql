e6f1bdb34ea600ee7c267d915147709ff370ad5c
Wesley <wesleys.felipe@gmail.com>
2015-11-30 21:22:52 -0200

    Carga de dados - sapatos masculinos

---
+++ flyway/src/main/resources/db/migration/V25__IMAGEM_PRODUTO_FEMININOS.sql
@@ -0,0 +1,54 @@
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (3, '../imagens/produtos/sapatos/feminino/produto1/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (3, '../imagens/produtos/sapatos/feminino/produto1/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (3, '../imagens/produtos/sapatos/feminino/produto1/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (3, '../imagens/produtos/sapatos/feminino/produto1/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (4, '../imagens/produtos/sapatos/feminino/produto2/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (4, '../imagens/produtos/sapatos/feminino/produto2/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (4, '../imagens/produtos/sapatos/feminino/produto2/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (4, '../imagens/produtos/sapatos/feminino/produto2/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (5, '../imagens/produtos/sapatos/feminino/produto3/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (5, '../imagens/produtos/sapatos/feminino/produto3/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (5, '../imagens/produtos/sapatos/feminino/produto3/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (5, '../imagens/produtos/sapatos/feminino/produto3/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (6, '../imagens/produtos/sapatos/feminino/produto4/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (6, '../imagens/produtos/sapatos/feminino/produto4/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (6, '../imagens/produtos/sapatos/feminino/produto4/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (6, '../imagens/produtos/sapatos/feminino/produto4/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (7, '../imagens/produtos/sapatos/feminino/produto5/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (7, '../imagens/produtos/sapatos/feminino/produto5/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (7, '../imagens/produtos/sapatos/feminino/produto5/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (7, '../imagens/produtos/sapatos/feminino/produto5/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (8, '../imagens/produtos/sapatos/feminino/produto6/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (8, '../imagens/produtos/sapatos/feminino/produto6/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (8, '../imagens/produtos/sapatos/feminino/produto6/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (8, '../imagens/produtos/sapatos/feminino/produto6/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (9, '../imagens/produtos/sapatos/feminino/produto7/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (9, '../imagens/produtos/sapatos/feminino/produto7/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (9, '../imagens/produtos/sapatos/feminino/produto7/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (9, '../imagens/produtos/sapatos/feminino/produto7/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (10, '../imagens/produtos/sapatos/feminino/produto8/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (10, '../imagens/produtos/sapatos/feminino/produto8/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (10, '../imagens/produtos/sapatos/feminino/produto8/3.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (11, '../imagens/produtos/sapatos/feminino/produto9/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (11, '../imagens/produtos/sapatos/feminino/produto9/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (11, '../imagens/produtos/sapatos/feminino/produto9/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (11, '../imagens/produtos/sapatos/feminino/produto9/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (12, '../imagens/produtos/sapatos/feminino/produto10/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (12, '../imagens/produtos/sapatos/feminino/produto10/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (12, '../imagens/produtos/sapatos/feminino/produto10/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (12, '../imagens/produtos/sapatos/feminino/produto10/4.jpg');
+
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (13, '../imagens/produtos/sapatos/feminino/produto11/1.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (13, '../imagens/produtos/sapatos/feminino/produto11/2.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (13, '../imagens/produtos/sapatos/feminino/produto11/3.jpg');
+INSERT INTO FDV_ONLINE.IMAGEM_PRODUTO (IDPRODUTO, IMAGEM) VALUES (13, '../imagens/produtos/sapatos/feminino/produto11/4.jpg');
+
