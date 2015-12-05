96844a767480e2930cb36beac0e658ad95dc7a98
Wesley <wesleys.felipe@gmail.com>
2015-12-05 21:11:55 -0200

    Calculando subtotais e totais no carrinho

--- forcadevendas/src/main/webapp/carrinho/carrinho.js
+++ forcadevendas/src/main/webapp/carrinho/carrinho.js
@@ -3,12 +3,55 @@
 
 	carrinhoModule.controller('CarrinhoController', [ '$scope',
 			'CarrinhoService', function($scope, CarrinhoService) {
-				$scope.produtos;
+		
+				$scope.itens;
 
 				var load = function() {
-					$scope.produtos = CarrinhoService.list();
+					$scope.itens = CarrinhoService.list();
 				}
 				load();
+				
+				$scope.calcularSubtotal = function(preco, qtde){
+					if(preco == null || qtde == null){
+						return '-';
+					}
+					return (preco * qtde).toFixed(2);
+				}
+				
+				$scope.calcularTotal = function(){
+					var total = 0;
+					$scope.itens.every(function(item){
+						var subtotal = $scope.calcularSubtotal(item.precoPromocional, item.quantidade);
+						if(subtotal === '-'){
+							return false;
+						}
+						total += parseFloat(subtotal);
+						return true;
+					});
+					
+					if(total == 0){
+						return '-';
+					}
+					
+					return total.toFixed(2);
+				}
+				
+				$scope.calcularQtdeTotal = function(){
+					var total = 0;
+					$scope.itens.every(function(item){
+						if(!item.quantidade){
+							return false;
+						}
+						total += parseInt(item.quantidade);
+						return true;
+					});
+					
+					if(total == 0){
+						return '-';
+					}
+					
+					return total;
+				}
 
 			} ]);
 
