8bcd00d3d501a4bb111349f0ce9e38cb97372c76
Wesley <wesleys.felipe@gmail.com>
2015-12-05 19:01:13 -0200

    Salvando carrinho no localstorage

--- forcadevendas/src/main/webapp/services/carrinho.service.js
+++ forcadevendas/src/main/webapp/services/carrinho.service.js
@@ -1,17 +1,19 @@
 (function(angular) {
     var app = angular.module('forcaDeVendas');
 
-    app.factory('CarrinhoService',function() {
-        var items = [];
+    app.factory('CarrinhoService',['$window', function($window) {
+        var itens = [];
         return {
             list: function() {
-                return items;
+            	itens = JSON.parse($window.localStorage['carrinho'] || '{}');
+                return itens;
             },
         	add : function(item){
-        		items.push(item);
+        		itens.push(item);
+        		$window.localStorage['carrinho'] = JSON.stringify(itens);
         	}
         }
-    });
+    }]);
 
     
 })(angular);
