759ae019fa349245665666b09d42d90b31c92eec
Wesley <wesleys.felipe@gmail.com>
2015-12-06 16:44:14 -0200

    Botão visualizar pedido na tela de listagem de pedidos

--- forcadevendas/src/main/webapp/pedidos/pedidos.js
+++ forcadevendas/src/main/webapp/pedidos/pedidos.js
@@ -1,8 +1,8 @@
 (function(angular) {
     var pedidosModule = angular.module('forcaDeVendas.pedidos', []);
 
-    pedidosModule.controller('PedidosController', ['$scope', 'PedidoService',
-        function($scope, PedidoService) {
+    pedidosModule.controller('PedidosController', ['$scope', '$window', 'PedidoService',
+        function($scope, $window, PedidoService) {
             var updateList = function updateList() {
                 $scope.pedidos = PedidoService.query();
             };
@@ -20,6 +20,10 @@
 
                 return formattedPrice;
             };
+            
+            $scope.visualizarPedido = function(idPedido){
+				$window.location.href = '#/pedido/'+ idPedido;
+			}
         }
     ]);
 })(angular);
